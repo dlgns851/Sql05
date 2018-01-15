7313dd544f7d08d98478a9f78944b71d2429da7b
BIT-USER <BIT-USER@DESKTOP-S48VTQI>
2018-01-15 15:29:18 +0900

    c

--- sql05.sql
+++ sql05.sql
@@ -5,19 +5,19 @@ select first_name,last_name,salary,department_name
 from employees em,departments de
 where em.DEPARTMENT_ID=de.DEPARTMENT_ID
       and em.hire_date=(select max(hire_date)
-                    from employees);
+                    	from employees);
 --문제2.
 --평균연봉(salary)이 가장 높은 부서 직원들의 직원번호(employee_id), 이름(firt_name),
 --성(last_name)과 업무(job_title), 연봉(salary)을 조회하시오.
 select employee_id,first_name,last_name,job_title,salary
 from employees em , jobs jo
 where department_id=(select department_id
-                    from employees
-                    group by department_id
-                    having avg(salary)= ( select max(sb.av)
-                                          from(select department_id,avg(salary) av
-                                               from employees
-                                               group by department_id)sb ))
+                     from employees
+                     group by department_id
+                     having avg(salary)= ( select max(sb.av)
+                                           from(select department_id,avg(salary) av
+                                                from employees
+                                                group by department_id)sb ))
        and    em.job_id=jo.job_id;
 
 
@@ -54,23 +54,21 @@ group by location_id;  --로케이션별 평균 연봉
 
 SELECT city 
 FROM locations
-where location_id=(
+where location_id=(select location_id
+        		   from departments de,(select department_id,avg(salary) su
+   		                        		from employees
+           		                		group by department_id) sb 
 
-        select location_id
-        from departments de,   (select department_id,avg(salary) su
-                        from employees
-                         group by department_id) sb 
+        		   where de.department_id=sb.department_id 
+                   group by location_id
+                   having avg(su)=(select max(sb.su)
+                      			   from (select location_id,avg(su) su
+                             	   		 from departments de,   (select department_id,avg(salary) su
+                                                    			 from employees
+                                                    			 group by department_id) sb 
 
-         where de.department_id=sb.department_id 
-        group by location_id
-        having avg(su)=(select max(sb.su)
-                        from (select location_id,avg(su) su
-                             from departments de,   (select department_id,avg(salary) su
-                                                    from employees
-                                                     group by department_id) sb 
-
-                              where de.department_id=sb.department_id
-                             group by location_id) sb ));
+                            			  where de.department_id=sb.department_id
+                             			  group by location_id) sb ));
 
 
 --문제5.
@@ -79,12 +77,10 @@ where location_id=(
 --(2) jobs 테이블에서 (1)에서나온 잡아이디에 속하는 잡타이틀 출력
 select job_title
 from jobs
-where job_id=(
-select job_id
-      from employees
-    group by job_id
-    having avg(salary)=(
-select max(sb.av)
-from (select job_id,avg(salary) av
-      from employees
-    group by job_id) sb));
+where job_id=(select job_id
+      		  from employees
+   			  group by job_id
+    		  having avg(salary)=(select max(sb.av)
+								  from (select job_id,avg(salary) av
+      									from employees
+    									group by job_id) sb));
